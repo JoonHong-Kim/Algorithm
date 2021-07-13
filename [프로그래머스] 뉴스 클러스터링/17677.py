ecb3b3cb999d9a6a58ee97c0ca58db03dbd48655
danielkim30433 <30318926+danielkim30433@users.noreply.github.com>
2021-07-13 19:11:14 +0900

    구현
    counter

---
+++ [프로그래머스] 뉴스 클러스터링/17677.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+def solution(str1, str2):
+    #대문자로 바꿔줌
+    new_str1=str1.upper()
+    new_str2=str2.upper()
+    st1=[]
+    st2=[]
+    #두개씩 묶어서 알파벳만 있으면 추가
+    for x,y in zip(new_str1,new_str1[1:]):
+        if (x+y).isalpha():
+            st1.append(x+y)
+    for x,y in zip(new_str2,new_str2[1:]):
+        if (x+y).isalpha():
+            st2.append(x+y)
+            
+    #둘다 0이면 1 출력
+    if not st1 and not st2:
+        return 65536
+    
+    # Counter 사용해서 교집합
+    ints=Counter(st1)&Counter(st2)
+    ints=len(list(ints.elements()))
+    unio=len(list(unio.elements()))-ints
+    
+    answer=int(ints/unio*65536)
+    return answer
