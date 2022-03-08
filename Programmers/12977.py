73b541defc364de0a781e8130669ff00bd00f6d2
JoonHong-Kim <danielkim30433@gmail.com>
2022-03-08 21:35:55 +0900

    Programmers: 12977

--- Programmers/12977.py
+++ Programmers/12977.py
@@ -1,23 +1,31 @@
 from itertools import combinations
 from typing import List
-def solution(nums:List[int]) -> int:
-    #제한사한이 적으니 제일 편한걸로..
-    def is_prime(num:int) -> bool:
+
+
+def solution(nums: List[int]) -> int:
+    # 제한사한이 적으니 제일 편한걸로..
+    def is_prime(num: int) -> bool:
         if num < 2:
             return False
-        for i in range(2,num):
+        for i in range(2, num):
             if num % i == 0:
                 return False
         return True
+
     answer = 0
-    all_combi=list(combinations(nums,3))
+    all_combi = list(combinations(nums, 3))
     for i in all_combi:
         if is_prime(sum(i)):
-            answer+=1
+            answer += 1
 
     return answer
 
-if __name__ == '__main__':
-    assert solution([1,2,3,4])==1, f"The solution result is {solution([1,2,3,4])} but it should be 1"
-    assert solution([1,2,7,6,4])==4, f"The solution result is {solution([1,2,7,6,4])} but it should be 4"
+
+if __name__ == "__main__":
+    assert (
+        solution([1, 2, 3, 4]) == 1
+    ), f"The solution result is {solution([1,2,3,4])} but it should be 1"
+    assert (
+        solution([1, 2, 7, 6, 4]) == 4
+    ), f"The solution result is {solution([1,2,7,6,4])} but it should be 4"
     print("All tests passed")
