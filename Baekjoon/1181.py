e42521b5444019a0e5ff29d5251b10eab362fbbb
JoonHong-Kim <danielkim30433@gmail.com>
2022-03-08 23:16:59 +0900

    Baekjoon: 1181

---
+++ Baekjoon/1181.py
@@ -0,0 +1,13 @@
+import sys
+
+if __name__=="__main__":
+    input=sys.stdin.readline
+    N=int(input())
+    words=[]
+    for i in range(N):
+        words.append(input().strip())
+    words=list(set(words))
+    words.sort()
+    words.sort(key=len)
+    for word in words:
+        print(word)
