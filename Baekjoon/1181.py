e11378a70ed87c05b8552eba1b4fde48265e69c1
JoonHong-Kim <danielkim30433@gmail.com>
2022-03-08 23:24:07 +0900

    reformat

--- Baekjoon/1181.py
+++ Baekjoon/1181.py
@@ -1,12 +1,12 @@
 import sys
 
-if __name__=="__main__":
-    input=sys.stdin.readline
-    N=int(input())
-    words=[]
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    N = int(input())
+    words = []
     for i in range(N):
         words.append(input().strip())
-    words=list(set(words))
+    words = list(set(words))
     words.sort()
     words.sort(key=len)
     for word in words:
--- Baekjoon/1259.py
+++ Baekjoon/1259.py
@@ -1,16 +1,18 @@
 import sys
 
-def pelindrome(num:str)->bool:
-    if num==num[::-1]:
+
+def pelindrome(num: str) -> bool:
+    if num == num[::-1]:
         return True
     else:
         return False
 
-if __name__=="__main__":
-    input=sys.stdin.readline
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
     while True:
-        num=input().strip()
-        if num=="0":
+        num = input().strip()
+        if num == "0":
             break
         if pelindrome(num):
             print("yes")
